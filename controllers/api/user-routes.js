5a13f61cf6c250b93ac477556e8cb9228e3a20ba
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-05-01 16:39:41 -0600

    added log in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,6 +54,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
 
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
@@ -70,6 +71,28 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/login', (req, res) => {
+    
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
