ac0d1a51a24cade7a88ef41b31f1ae9ea76d4cae
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-05-02 15:51:41 -0600

    working on sign up

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,7 +43,14 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(UserData => res.json(UserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -85,15 +92,32 @@ router.post('/login', (req, res) => {
         }
 
         const validPassword = dbUserData.checkPassword(req.body.password);
+        
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
 
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
         res.render('dashboard', { user: dbUserData, message: 'You are now logged in!' });
     });
 });
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
     User.destroy({
