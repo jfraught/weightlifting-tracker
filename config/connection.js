ed6a0c7fa5a0d5f3babd018db8066381410d0ba2
gracianx <graciana.valles@gmail.com>
2021-05-05 21:43:47 -0600

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
-});
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+
 
 module.exports = sequelize;
