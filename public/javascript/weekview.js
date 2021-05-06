1d29ffbbaa3861a4e0bb49b348c7ea9a98a48948
Jaime Cedillo <74879881+jaimecedillo@users.noreply.github.com>
2021-05-05 21:02:28 -0600

    added weekview.js

---
+++ public/javascript/weekview.js
@@ -0,0 +1,7 @@
+
+
+const resetButton = document.querySelector('#reset-button');
+resetButton.onclick = function () {
+
+  console.log("button clicked");
+}
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const seedUsers = require('./user-seeds');
 const seedWorkouts = require('./workout-seeds');
-
+require('dotenv').config();
+console.log(process.env)
 const sequelize = require('../config/connection');
 const seedAll = async () => {
     await sequelize.sync({ force: true });
